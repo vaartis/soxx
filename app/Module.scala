ed68ed912c9016fa5d1becada5af2b1f1ce1adee
Ekaterina Vaartis <vaartis@cock.li>
2018-04-22 17:19:35 +0300

    Remove unused imports from Module

--- app/Module.scala
+++ app/Module.scala
@@ -2,17 +2,12 @@ import com.google.inject.AbstractModule
 import play.api.libs.concurrent.AkkaGuiceSupport
 
 import soxx.mongowrapper.Mongo
-
-import akka.actor._
-
 import soxx.scrappers._
-import org.mongodb.scala._
 
 class Module extends AbstractModule with AkkaGuiceSupport {
   def configure = {
     bind(classOf[Mongo]).asEagerSingleton()
 
     bindActor[ScrapperSupervisor]("scrapper-supervisor")
-
   }
 }
