2531e5ee1e23d45da3cb09c376235dad57677c97
Ekaterina Vaartis <vaartis@cock.li>
2018-05-11 21:35:09 +0300

    Add a mongodb registry with the Image class in it

--- app/Mongo.scala
+++ app/Mongo.scala
@@ -3,17 +3,24 @@ package soxx.mongowrapper
 import javax.inject._
 
 import org.mongodb.scala.MongoClient
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.bson.codecs.configuration.CodecRegistries.{fromRegistries, fromProviders}
+
 import play.api.inject.ApplicationLifecycle
 import scala.concurrent.{ ExecutionContext, Future }
+
+import soxx.scrappers.Image
 
 @Singleton
 class Mongo @Inject() (implicit lifecycle: ApplicationLifecycle, ec: ExecutionContext) {
   val client: MongoClient = MongoClient()
-  lazy val db = client.getDatabase("soxx")
+
+  val codecRegistry = fromRegistries(fromProviders(classOf[Image]), DEFAULT_CODEC_REGISTRY)
+
+  lazy val db = client.getDatabase("soxx").withCodecRegistry(codecRegistry)
 
   lifecycle.addStopHook { () =>
-    client.close()
-
-    Future { }
+    Future { client.close() }
   }
 }
