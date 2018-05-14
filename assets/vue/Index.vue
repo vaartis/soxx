1c020b7e95fd7de386034909cfb5a8a4a77eb41f
Ekaterina Vaartis <vaartis@cock.li>
2018-05-14 19:49:34 +0300

    Show the origin of image, it's favicon and original post link

--- assets/vue/Index.vue
+++ assets/vue/Index.vue
@@ -1,18 +1,49 @@
 <template>
     <div class="card-columns">
-        <div class="card img-card" v-for="image in images" key="image._id" v-bind:style="`background-image: url(${image.originalImage})`" />
+        <div class="card" v-for="image in images" v-bind:key="image._id">
+            <!-- Hacky but works! Looks beter then object-fit too  -->
+            <!-- Just using the first "from" may not be the best idea -->
+            <div class="card-img-top img-card"
+                 v-bind:style="`background-image: url(${image.from[0].image})`" />
+            <div class="card-body">
+                <div class="card-header">
+                    <a v-for="from in image.from" v-bind:key="from.name"
+                       v-bind:href="from.post" v-bind:title="from.name">
+                        <img v-bind:src="favicon(from.name)" />
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+ import _ from "lodash";
+ import Vue from "vue"
+
  export default {
      data() {
          return {
+             imboard_info: {},
              images: []
          }
      },
 
+     methods: {
+         favicon(imgFrom) {
+             return this.imboard_info[imgFrom].favicon;
+         }
+     },
+
      mounted() {
+         fetch("/imboard_info")
+             .then(resp => {
+                 resp.json().then(boards => {
+                     _.forEach(boards, (board) => {
+                         Vue.set(this.imboard_info, board._id, _.omit(board, "_id"));
+                     })
+                 })
+             })
          fetch("/images")
              .then(resp => {
                  resp.json().then(imgs => {
@@ -29,8 +60,8 @@
  }
 
  .img-card {
-     width: 20em;
      height: 30em;
+     width: auto;
      position: relative;
      background-position: 50% 50%;
      background-repeat: no-repeat;
