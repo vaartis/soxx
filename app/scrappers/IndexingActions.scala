180afa0e24e47cf7e67a941639e86d349898b308
Ekaterina Vaartis <vaartis@cock.li>
2018-05-17 22:13:32 +0300

    Add toPage to StartIndexing

--- app/scrappers/IndexingActions.scala
+++ app/scrappers/IndexingActions.scala
@@ -1,5 +1,5 @@
 package soxx.scrappers
 
 abstract class IndexingAction
-case class StartIndexing(fromPage: Int = 1) extends IndexingAction
+case class StartIndexing(fromPage: Int = 1, toPage: Option[Int] = None) extends IndexingAction
 case object StopIndexing extends IndexingAction
