f9ad7de0f03fd06354fd0bdf5664b4924eb6ceff
vaartis <vaartis@cock.li>
2018-06-29 16:58:29 +0000

    Make indexing actions scrapper actions, add scrapper status message

--- app/scrappers/IndexingActions.scala
+++
@@ -1,6 +0,0 @@
-package soxx.scrappers
-
-abstract class IndexingAction
-case class StartIndexing(fromPage: Int = 1, toPage: Option[Int] = None) extends IndexingAction
-case object StopIndexing extends IndexingAction
-case object IsIndexing extends IndexingAction
---
+++ app/scrappers/ScrapperActions.scala
@@ -0,0 +1,14 @@
+package soxx.scrappers
+
+abstract class ScrapperAction
+case class StartIndexing(fromPage: Int = 1, toPage: Option[Int] = None) extends ScrapperAction
+case object StopIndexing extends ScrapperAction
+
+case object StartDownloading extends ScrapperAction
+case object StopDownloading extends ScrapperAction
+
+case object ScrapperStatusMsg extends ScrapperAction
+case class ScrapperStatus(isIndexing: Boolean, isDownloading: Boolean)
+object ScrapperStatus {
+  implicit val spStatusFormat = play.api.libs.json.Json.format[ScrapperStatus]
+}
