8ad9f1c6afbbbb856f49134d69b5b0ed36acfe61
Ekaterina Vaartis <vaartis@cock.li>
2018-07-17 13:02:08 +0300

    Make SoxxController actually use the downloadDirectory option

--- app/controllers/SoxxController.scala
+++ app/controllers/SoxxController.scala
@@ -3,12 +3,13 @@ package controllers
 import scala.concurrent._
 import javax.inject._
 
+import play.api.Configuration
 import play.api.mvc._
 
 @Singleton
 class SoxxController @Inject()(
   cc: ControllerComponents
-)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+)(implicit ec: ExecutionContext, config: Configuration) extends AbstractController(cc) {
 
   def index = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
@@ -21,7 +22,7 @@ class SoxxController @Inject()(
   def image_files(name: String) = Action { implicit request: Request[AnyContent] =>
     import java.nio.file.{Files, Paths}
 
-    val p = Paths.get("images", name)
+    val p = Paths.get(config.get[String]("soxx.scrappers.downloadDirectory"), name)
     if (Files.exists(p)) { Ok.sendFile(p.toFile, true) } else { NotFound(f"Image $name not found") }
   }
 
